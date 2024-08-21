02937299df8bc19d268643c01e1df9a537f73ea5
Riyane <111461393+Purpose-Dev@users.noreply.github.com>
2024-08-21 19:00:30 +0200

    add: Base Files containing tsconfig files, prettier config, eslint config and typedoc

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+
+module.exports = [
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      'no-async-promise-executor': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-var-requires': 'error',
+    },
+    ignores: [
+      '.circleci',
+      '.github',
+      'node_modules',
+      'dist',
+      'docs',
+      'examples',
+    ],
+  },
+];
