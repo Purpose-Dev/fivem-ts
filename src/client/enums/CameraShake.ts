24c4b3c4db2f539d1335ca2e45960a32fa8de768
Riyane <111461393+Purpose-Dev@users.noreply.github.com>
2024-08-24 18:18:05 +0200

    feat: Add new enums for ExplosionType, CloudHat, VehicleDrivingFlags, CameraShake, DrivingStyle, CursorType, ForceType, Font, CheckboxStyle, and Gender
    - Bump package version to 0.3.0 in package.json
    - Update client enums index to include new enums

---
+++ src/client/enums/CameraShake.ts
@@ -0,0 +1,13 @@
+export enum CameraShake {
+    DeathFail = 'DEATH_FAIL_IN_EFFECT_SHAKE',
+    Drunk = 'DRUNK_SHAKE',
+    FamilyDrugTrip = 'FAMILY5_DRUG_TRIP_SHAKE',
+    Hand = 'HAND_SHAKE',
+    Jolt = 'JOLT_SHAKE',
+    LargeExplosion = 'LARGE_EXPLOSION_SHAKE',
+    MediumExplosion = 'MEDIUM_EXPLOSION_SHAKE',
+    SmallExplosion = 'SMALL_EXPLOSION_SHAKE',
+    RoadVibration = 'ROAD_VIBRATION_SHAKE',
+    SkyDiving = 'SKY_DIVING_SHAKE',
+    Vibrate = 'VIBRATE_SHAKE',
+}
---
+++ src/client/enums/CursorType.ts
@@ -0,0 +1,15 @@
+export enum CursorType {
+    None = 0,
+    Normal = 1,
+    TransparentNormal = 2,
+    PreGrab = 3,
+    Grab = 4,
+    MiddleFinger = 5,
+    LeftArrow = 6,
+    RightArrow = 7,
+    UpArrow = 8,
+    DownArrow = 9,
+    HorizontalExpand = 10,
+    Add = 11,
+    Remove = 12,
+}
