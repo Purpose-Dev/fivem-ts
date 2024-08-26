ed54208cd64ba13218a7faa415b782dca6812e24
Riyane <111461393+Purpose-Dev@users.noreply.github.com>
2024-08-26 19:48:45 +0200

    feat: Add new enums for MaterialEnum, ScreenEffect, Bone, SpeechModifier, HudComponent, IntersectionsOptions, BlipType, VehicleIndicatorsLights, RagdollType, and RopeType

---
+++ src/client/enums/BlipType.ts
@@ -0,0 +1,14 @@
+export enum BlipType {
+    Vehicle = 1,
+    Ped = 2,
+    Object = 3,
+    Destination = 4,
+    Cont = 5,
+    PickupUnk = 6,
+    Radius = 7,
+    Pickup = 8,
+    Cop = 9,
+    Area = 10,
+    Gallery = 11,
+    PickupObject = 12,
+}
---
+++ src/client/enums/VehicleIndicatorsLights.ts
@@ -0,0 +1,8 @@
+export enum VehicleIndicatorsLights {
+    None = 0,
+    BlinkLeft = 1,
+    BlinkRight = 2,
+    BlinkBoth = 3,
+    StaticBoth = 8,
+    Interior = 64,
+}
