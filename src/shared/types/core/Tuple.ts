80655df6c1456168db4a617c3f615958f8eca369
Riyane <111461393+Purpose-Dev@users.noreply.github.com>
2024-08-29 01:51:53 +0200

    feat: Add core types for AbstractConstructor, Constructor, Dictionary, Tuple, and WithID

---
+++ src/shared/types/core/Tuple.ts
@@ -0,0 +1,1 @@
+export type Tuple<T1, T2> = [T1, T2];
---
+++ src/shared/types/core/index.ts
@@ -0,0 +1,5 @@
+export { AbstractConstructor } from './AbstractConstructor';
+export { Constructor } from './Constructor';
+export { Dictionary } from './Dictionary';
+export { Tuple } from './Tuple';
+export { WithID } from './WithID';
