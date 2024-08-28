bf1dd1c9a6ca3ee606d6f9cf99f5258728ad3dcb
Riyane <111461393+Purpose-Dev@users.noreply.github.com>
2024-08-29 01:59:08 +0200

    feat: Add serializeToJSON utility function and export from shared utils

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -9,4 +9,5 @@ export {
 export { FMath } from './FMath';
 export { FString } from './FString';
 export { isDuplicityVersion } from './isDuplicityVersion';
+export { serializeToJSON } from './serializeToJSON';
 export { NumerableVector3, Vec3, Vector3 } from './Vector3';
---
+++ src/shared/utils/serializeToJSON.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function serializeToJSON(data: any): string {
+    return JSON.stringify(data);
+}
