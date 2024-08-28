0c93518fa96f28f0180277cda28eeda557224f81
Riyane <111461393+Purpose-Dev@users.noreply.github.com>
2024-08-29 01:55:29 +0200

    feat: Add utility types including ArrayOrSingle, Deferred, Errorable, Maybe, Nullable, and PromiseOrValue to shared types

---
+++ src/shared/types/utilities/Deferred.ts
@@ -0,0 +1,5 @@
+export type Deferred<T> = {
+    promise: Promise<T>;
+    resolve: (value: T | PromiseLike<T>) => void;
+    reject: (reason?: unknown) => void;
+};
---
+++ src/shared/types/utilities/Errorable.ts
@@ -0,0 +1,1 @@
+export type Errorable<T> = T | Error;
