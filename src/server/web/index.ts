032e24ec03f1a7a4af117e4395148b18ce5db56f
Riyane <111461393+Purpose-Dev@users.noreply.github.com>
2024-09-02 18:20:00 +0200

    feat: update index exports to include HTTP request helpers and makeRequest function

--- src/server/web/index.ts
+++ src/server/web/index.ts
@@ -1,5 +1,7 @@
 export { Controller } from './Controller';
 export { HttpRequest } from './HttpRequest';
 export { HttpResponse } from './HttpResponse';
+export { makeRequest, RequestData, RequestHeaders, RequestMethods } from './makeRequest';
+export { del, get, post, put } from './requests';
 export { Route } from './Route';
 export { startServer } from './startServer';
