491c552170fd9d78754105afd9dea5585d610814
Riyane <111461393+Purpose-Dev@users.noreply.github.com>
2024-09-03 19:39:03 +0200

    feat: update client index exports to include Container and Effects modules

--- src/client/ui/index.ts
+++ src/client/ui/index.ts
@@ -1,5 +1,7 @@
 export * from './interfaces';
 
+export { Container } from './Container';
+export { Effects } from './Effects';
 export { Fading } from './Fading';
 export { Hud } from './Hud';
 export { LoadingPrompt } from './LoadingPrompt';
