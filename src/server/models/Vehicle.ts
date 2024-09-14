9c12c195362fc54738623be8ca7b03285f8fbaca
Purpose <111461393+Purpose-Dev@users.noreply.github.com>
2024-09-14 07:31:44 +0200

    Add Vehicle class to models directory
    Introduce the Vehicle class extending the Entity class, ensuring the instantiated ID corresponds to a vehicle type. This serves as groundwork for further methods handling vehicle-specific logic.

---
+++ src/server/models/Vehicle.ts
@@ -0,0 +1,16 @@
+import { Entity } from '.';
+
+// @todo: Add Methods related to Vehicle
+
+/**
+ * Vehicle class that extends the Entity class.
+ * Represents a vehicle entity.
+ */
+export class Vehicle extends Entity {
+    constructor(id: number) {
+        if (!(GetEntityType(id) === 2)) {
+            throw new Error('Entity type is not a vehicle');
+        }
+        super(id);
+    }
+}
