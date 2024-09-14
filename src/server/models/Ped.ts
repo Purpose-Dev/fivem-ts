0c4da22711e1b607a9f6ed9498083bebdb30b89e
Purpose <111461393+Purpose-Dev@users.noreply.github.com>
2024-09-14 07:31:24 +0200

    Add Ped.ts model for pedestrian entity interactions
    Implemented the Ped class extending the Entity class to represent pedestrians. The class includes methods for checking if the pedestrian is in a vehicle, a player, handcuffed, ragdoll, strafing, or using action mode. Future work will involve adding more specific methods related to pedestrian actions.

---
+++ src/server/models/Ped.ts
@@ -0,0 +1,54 @@
+import { Entity, Vehicle } from '.';
+
+/**
+ * The `Ped` class represents a pedestrian entity.
+ *
+ * This class extends the `Entity` class and provides an interface for interacting with pedestrian entities.
+ * The constructor takes the entity ID as a parameter and ensures the entity type is a pedestrian.
+ *
+ * @extends {Entity}
+ */
+export class Ped extends Entity {
+    constructor(id: number) {
+        if (!(GetEntityType(id) === 1)) {
+            throw new Error('Entity type is not a ped');
+        }
+        super(id);
+    }
+
+    public get CurrentVehicle(): Vehicle {
+        const vehicle: Vehicle = new Vehicle(GetVehiclePedIsIn(this.Id, false));
+        return vehicle.exists() ? vehicle : null;
+    }
+
+    public get LastVehicle(): Vehicle {
+        const vehicle: Vehicle = new Vehicle(GetVehiclePedIsIn(this.Id, true));
+        return vehicle.exists() ? vehicle : null;
+    }
+
+    public get IsInVehicle(): boolean {
+        return !!this.CurrentVehicle;
+    }
+
+    public get IsAPlayer(): boolean {
+        return IsPedAPlayer(this.Id);
+    }
+
+    public get IsHandcuffed(): boolean {
+        return IsPedHandcuffed(this.Id);
+    }
+
+    public get IsRagdoll(): boolean {
+        return IsPedRagdoll(this.Id);
+    }
+
+    public get IsStrafing(): boolean {
+        return IsPedStrafing(this.Id);
+    }
+
+    public get IsUsingActionMode(): boolean {
+        return IsPedUsingActionMode(this.Id);
+    }
+
+    // @todo: Add Methods related to Ped
+}
