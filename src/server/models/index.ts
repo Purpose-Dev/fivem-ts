e901d45d6c29eb6f6d2e0711aa3b5901fb4dc66a
Purpose <111461393+Purpose-Dev@users.noreply.github.com>
2024-09-14 07:32:01 +0200

    Add Ped and Vehicle exports to models index
    This commit adds exports for the Ped and Vehicle classes in the models index file. This ensures that these classes can be imported and used in other parts of the application.

--- src/server/models/index.ts
+++ src/server/models/index.ts
@@ -1,3 +1,5 @@
 export { Bucket } from './Bucket';
 export { BucketCollection, bucketCollection } from './BucketCollection';
 export { Entity } from './Entity';
+export { Ped } from './Ped';
+export { Vehicle } from './Vehicle';
