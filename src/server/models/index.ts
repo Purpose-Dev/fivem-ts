c2598a0f27693f185fbf5145d3bd2789130f5b90
Purpose <111461393+Purpose-Dev@users.noreply.github.com>
2024-09-30 18:25:08 +0200

    feat: Add CustomVehicleColour and VehicleColours exports
    Modified the exports in the index.ts file to include CustomVehicleColour and VehicleColours from the Vehicle module. This update allows these additional features to be available for use in other parts of the application.
    
    Signed-off-by: Purpose <111461393+Purpose-Dev@users.noreply.github.com>

--- src/server/models/index.ts
+++ src/server/models/index.ts
@@ -2,4 +2,4 @@ export { Bucket } from './Bucket';
 export { BucketCollection, bucketCollection } from './BucketCollection';
 export { Entity } from './Entity';
 export { Ped } from './Ped';
-export { Vehicle } from './Vehicle';
+export { CustomVehicleColour, Vehicle, VehicleColours } from './Vehicle';
