42737a5f32ed52daf8954314733c017e93dc9b97
Purpose <111461393+Purpose-Dev@users.noreply.github.com>
2024-10-07 02:16:20 +0200

    Add utility functions to retrieve random enum entries, keys, and values
    Introduced three new utility functions: `getRandomEnumKey`, `getRandomEnumEntry`, and `getRandomEnumValue`. These functions facilitate the retrieval of random keys, key-value pairs, and values from a given enum type respectively. The updates include corresponding type filters and index file exports for the new functions.

---
+++ packages/shared/src/utils/enum/getRandomEnumKey.ts
@@ -0,0 +1,14 @@
+/**
+ * Returns a random key from the given enum type.
+ *
+ * @template T
+ *
+ * @param {T} enumType - The enum type from which to select a random key
+ *
+ * @return {keyof T} A randomly selected key from the provided enum type.
+ */
+export function getRandomEnumKey<T extends object>(enumType: T): keyof T {
+    const keys = Object.keys(enumType).filter(key => isNaN(Number(key))) as Array<keyof T>;
+    const randomIndex: number = Math.floor(Math.random() * keys.length);
+    return keys[randomIndex];
+}
---
+++ packages/shared/src/utils/enum/index.ts
@@ -0,0 +1,3 @@
+export { getRandomEnumEntry } from './getRandomEnumEntry';
+export { getRandomEnumKey } from './getRandomEnumKey';
+export { getRandomEnumValue } from './getRandomEnumValue';
