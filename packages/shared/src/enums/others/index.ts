553a0bb6bfb0923ae3de3d8eafc9823d8666d10b
Purpose <111461393+Purpose-Dev@users.noreply.github.com>
2024-10-07 02:25:52 +0200

    add: Add BlipSprites enum for radar and map interfaces
    Added the BlipSprites enum to represent various blip types on radar or map interfaces. This allows for customization of the appearance of different elements on the radar or map.

---
+++ packages/shared/src/enums/others/index.ts
@@ -0,0 +1,4 @@
+export { BlipSprites } from './BlipSprites';
+export { EntityOrphanMode } from './EntityOrphanMode';
+export { RadioStation } from './RadioStation';
+export { WeatherEnum } from './WeatherEnum';
