b5fd9422d75fd8023d0059a7c864a7d287064274
Purpose <111461393+Purpose-Dev@users.noreply.github.com>
2024-10-07 02:26:38 +0200

    Add PedType enum to shared package
    Introduced a new PedType enum to categorize different pedestrian types. This includes civilian males, civilian females, and animals, enhancing the system's ability to differentiate these entities.

---
+++ packages/shared/src/enums/peds/PedType.ts
@@ -0,0 +1,15 @@
+/**
+ * Enum representing different types of pedestrians.
+ *
+ * PedType enumerates the possible pedestrian types in the system:
+ * - `CivMale`: Represents a civilian male.
+ * - `CivFemale`: Represents a civilian female.
+ * - `Animal`: Represents an animal.
+ *
+ * @enum{string}
+ */
+export enum PedType {
+    CivMale = 'CIVMALE',
+    CivFemale = 'CIVFEMALE',
+    Animal = 'ANIMAL',
+}
---
+++ packages/shared/src/enums/peds/index.ts
@@ -0,0 +1,1 @@
+export { PedType } from './PedType';
