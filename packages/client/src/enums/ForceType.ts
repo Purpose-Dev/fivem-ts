b9fc574e5aa67c46069539544a47ac50c10d011a
Johann Kraus <56100605+JericoFX@users.noreply.github.com>
2024-10-08 15:41:43 -0300

    add some info

---
+++ packages/client/src/enums/ForceType.ts
@@ -0,0 +1,13 @@
+/**
+ * Enumeration representing different types of forces.
+ *
+ * ForceType is used to define constants that represent various force behaviors.
+ */
+export enum ForceType {
+  MinForce,
+  MaxForceRot,
+  MinForce2,
+  MaxForceRot2,
+  ForceNoRot,
+  ForceRotPlusForce,
+}
---
+++ packages/client/src/utils/index.ts
@@ -0,0 +1,5 @@
+export * from './Quaternions';
+export * from './Colors';
+export * from './Points';
+export * from './PointsF';
+export * from './Size';
