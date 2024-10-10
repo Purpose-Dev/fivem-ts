27dc5156783cee6fb051cf54d804af4dcb5a1db8
Purpose <111461393+Purpose-Dev@users.noreply.github.com>
2024-10-10 23:01:54 +0200

    Add PedsEnum enumeration for game pedestrian types
    Introduces a new PedsEnum enumeration to represent various game pedestrians. This change aids in achieving stronger typing for parameters and return values, replacing ambiguous strings or numbers. The enum can be extended by developers to include additional predefined pedestrians as needed.

---
+++ packages/shared/src/enums/peds/PedsEnum.ts
@@ -0,0 +1,48 @@
+// @todo: finish peds enum
+
+/**
+ * The `PedsEnum` enumeration represents game peds.
+ *
+ * This can be especially useful for strongly typing certain
+ * parameters or return values rather than using strings or numbers.
+ *
+ * Developers can extend this `enum` as needed for additional
+ * predefined peds.
+ *
+ * @enum{string}
+ */
+export enum PedsEnum {
+    // Ambient Female
+    AmbientFemaleMBeach01 = 'a_f_m_beach_01',
+    AmbientFemaleMBevHills01 = 'a_f_m_bevhills_01',
+    AmbientFemaleMBevHills02 = 'a_f_m_bevhills_02',
+    AmbientFemaleMBodyBuild01 = 'a_f_m_bodybuild_01',
+    AmbientFemaleMBusiness02 = 'a_f_m_business_02',
+    AmbientFemaleMDownTown01 = 'a_f_m_downtown_01',
+    AmbientFemaleMEastSA01 = 'a_f_m_eastsa_01',
+    AmbientFemaleMEastSA02 = 'a_f_m_eastsa_02',
+    AmbientFemaleMFatBla01 = 'a_f_m_fatbla_01',
+    AmbientFemaleMFatCult01 = 'a_f_m_fatcult_01',
+    AmbientFemaleMFatWhite01 = 'a_f_m_fatwhite_01',
+    AmbientFemaleMKTown01 = 'a_f_m_ktown_01',
+    AmbientFemaleMKTown02 = 'a_f_m_ktown_02',
+    AmbientFemaleMProlhost01 = 'a_f_m_prolhost_01',
+    AmbientFemaleMSalton01 = 'a_f_m_salton_01',
+    AmbientFemaleMSkidrow01 = 'a_f_m_skidrow_01',
+    AmbientFemaleMSoucent01 = 'a_f_m_soucent_01',
+    AmbientFemaleMSoucent02 = 'a_f_m_soucent_02',
+    AmbientFemaleMSoucentMc01 = 'a_f_m_soucentmc_01',
+    AmbientFemaleMTourist01 = 'a_f_m_tourist_01',
+    AmbientFemaleMTramp01 = 'a_f_m_tramp_01',
+    AmbientFemaleMTrampbeac01 = 'a_f_m_trampbeac_01',
+    AmbientFemaleOGenstreet01 = 'a_f_o_genstreet_01',
+    AmbientFemaleOIndian01 = 'a_f_o_indian_01',
+    AmbientFemaleOKTown01 = 'a_f_o_ktown_01',
+    AmbientFemaleOKSalton01 = 'a_f_o_salton_01',
+    AmbientFemaleOSoucent01 = 'a_f_o_soucent_01',
+    AmbientFemaleOSoucent02 = 'a_f_o_soucent_02',
+    AmbientFemaleYBeach01 = 'a_f_y_beach_01',
+    AmbientFemaleYBevhills01 = 'a_f_y_bevhills_01',
+    AmbientFemaleYBevhills02 = 'a_f_y_bevhills_02',
+    AmbientFemaleYBevhills03 = 'a_f_y_bevhills_03',
+}
