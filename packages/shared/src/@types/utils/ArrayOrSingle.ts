785a066e1d3bd0620844f57ed9d951435e0178f4
Purpose <111461393+Purpose-Dev@users.noreply.github.com>
2024-10-11 08:17:34 +0200

    feat(types): add common utility types
    Introduced new utility types for better type handling: ArrayOrSingle, Errorable, Maybe, Nullable, and PromiseOrValue. Updated the index file to export these types.

---
+++ packages/shared/src/@types/utils/ArrayOrSingle.ts
@@ -0,0 +1,1 @@
+export type ArrayOrSingle<T> = T | T[];
---
+++ packages/shared/src/@types/utils/index.ts
@@ -0,0 +1,5 @@
+export type { ArrayOrSingle } from './ArrayOrSingle';
+export type { Errorable } from './Errorable';
+export type { Maybe } from './Maybe';
+export type { Nullable } from './Nullable';
+export type { PromiseOrValue } from './PromiseOrValue';
