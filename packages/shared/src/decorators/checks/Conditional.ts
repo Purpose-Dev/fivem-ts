a36352596929f94f19fa3b868e7bc46af905b825
Purpose <111461393+Purpose-Dev@users.noreply.github.com>
2024-10-11 08:35:21 +0200

    feat(decorators): add Conditional decorator
    Introduce `@Conditional` decorator for conditionally executing a method based on a provided condition function, logging a warning if the condition is not met.

---
+++ packages/shared/src/decorators/checks/Conditional.ts
@@ -0,0 +1,39 @@
+/**
+ * A decorator that conditionally executes a method based on a provided condition function.
+ *
+ * The `@Conditional` decorator wraps the decorated method and only executes it if the provided condition function
+ * returns `true` for the given arguments. If the condition is not met, the method is not executed and a warning
+ * message is logged to the console.
+ *
+ * @example
+ * ```ts
+ * class Example {
+ *     @Conditional((active) => active === true)
+ *     public performAction(active: boolean) {
+ *         console.log(`Action performed! with active value is ${active}`);
+ *     }
+ * }
+ *
+ * const example = new Example();
+ * example.performAction(false); // Condition not met for function: performAction
+ * example.performAction(true); // Action performed! with active value is true
+ * ```
+ *
+ * @param conditionFunc A function that takes the method's arguments and returns a boolean indicating whether the method should be executed.
+ *
+ */
+export function Conditional(conditionFunc: (...args: unknown[]) => boolean) {
+    return function (_target: unknown, propertyKey: string, descriptor: PropertyDescriptor) {
+        const originalMethod = descriptor.value;
+
+        descriptor.value = function (...args: unknown[]) {
+            if (conditionFunc(...args)) {
+                return originalMethod.apply(this, args);
+            } else {
+                console.warn(`Condition not met for function: ${propertyKey}`);
+            }
+        };
+
+        console.log(`Registered conditional execution for function: ${propertyKey}`);
+    };
+}
---
+++ packages/shared/src/decorators/checks/index.ts
@@ -0,0 +1,1 @@
+export { Conditional } from './Conditional';
