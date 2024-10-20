8ff01f370114e43a98e1d6b86ffcaf4d2ab9637f
Purpose <111461393+Purpose-Dev@users.noreply.github.com>
2024-10-20 04:58:46 +0200

    style(eslint): correct indentations in eslint configuration
    ---
    Adjusted the indentation for better readability and consistency in eslint.config.js.

--- eslint.config.js
+++ eslint.config.js
@@ -2,28 +2,28 @@ const tsParser = require('@typescript-eslint/parser');
 const tsPlugin = require('@typescript-eslint/eslint-plugin');
 
 module.exports = [
-  {
-    files: ['packages/**/*.ts'],
-    languageOptions: {
-      parser: tsParser,
+    {
+        files: ['packages/**/*.ts'],
+        languageOptions: {
+            parser: tsParser,
+        },
+        plugins: {
+            '@typescript-eslint': tsPlugin,
+        },
+        rules: {
+            'no-async-promise-executor': 'off',
+            '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+            '@typescript-eslint/explicit-function-return-type': 'off',
+            '@typescript-eslint/no-explicit-any': 'warn',
+            '@typescript-eslint/no-var-requires': 'error',
+        },
+        ignores: [
+            '.circleci',
+            '.github',
+            'node_modules',
+            'dist',
+            'docs',
+            'examples',
+        ],
     },
-    plugins: {
-      '@typescript-eslint': tsPlugin,
-    },
-    rules: {
-      'no-async-promise-executor': 'off',
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-var-requires': 'error',
-    },
-    ignores: [
-      '.circleci',
-      '.github',
-      'node_modules',
-      'dist',
-      'docs',
-      'examples',
-    ],
-  },
 ];
