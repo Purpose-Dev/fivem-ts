64a12ab5a819d1aa70807be49f6fc97b3bb8086f
Purpose <111461393+Purpose-Dev@users.noreply.github.com>
2024-10-30 03:22:50 +0100

    feat(utils): add UUID generation and buffer serialization functions
    Introduced utility functions for UUID v4 and v5 generation and data serialization to buffer. These functions are crucial for ensuring unique identifiers and efficient data handling within the server package.

---
+++ packages/server/src/utils/index.ts
@@ -0,0 +1,2 @@
+export { serializeToBuffer } from './serializeToBuffer';
+export { uuidv4, uuidv5 } from './UUID';
---
+++ packages/server/src/utils/serializeToBuffer.ts
@@ -0,0 +1,9 @@
+import { serializeToJSON } from '@fivem-ts/shared';
+
+export function serializeToBuffer(data: unknown): Buffer {
+    if (Buffer.isBuffer(data)) {
+        return data;
+    }
+
+    return Buffer.from(serializeToJSON(data), 'utf8');
+}
