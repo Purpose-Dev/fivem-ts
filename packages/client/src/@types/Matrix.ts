024c541a2311b5e533d460da949b904f588435a4
Purpose <111461393+Purpose-Dev@users.noreply.github.com>
2024-12-18 13:11:35 +0100

    `feat(types): add type definitions for Vector, Matrix, and HandlingData`
    ---
    Added new type definitions for `Vector`, `Matrix`, and `HandlingData`. Also included exports for these types in the index file to streamline usage.

---
+++ packages/client/src/@types/Matrix.ts
@@ -0,0 +1,3 @@
+import { Vector } from './Vector';
+
+export type Matrix = [Vector, Vector, Vector, Vector];
---
+++ packages/client/src/@types/index.ts
@@ -0,0 +1,3 @@
+export type { HandlingData } from './HandlingData';
+export type { Matrix } from './Matrix';
+export type { Vector } from './Vector';
