233bad8a761028795e835b577e1300914963af44
Purpose <111461393+Purpose-Dev@users.noreply.github.com>
2024-12-18 23:37:04 +0100

    feat(enums): add new enums for client application
    ---
    Added multiple enums (`Gender`, `DrivingStyle`, `HandlingKeys`, `ExplosionType`, `HelmetType`, `Font`, `ForceType`) to enhance the client application's type system and improve code readability and maintainability. Updated the `enums/index.ts` to export the newly created enums.

---
+++ packages/client/src/enums/ForceType.ts
@@ -0,0 +1,15 @@
+/**
+ * Enumeration representing different types of forces.
+ *
+ * ForceType is used to define constants that represent various force behaviors.
+ *
+ * @enum{number}
+ */
+export enum ForceType {
+    MinForce,
+    MaxForceRot,
+    MinForce2,
+    MaxForceRot2,
+    ForceNoRot,
+    ForceRotPlusForce,
+}
---
+++ packages/client/src/enums/Gender.ts
@@ -0,0 +1,12 @@
+/**
+ * Enumeration representing gender.
+ * It contains the following values:
+ * - `Male`
+ * - `Female`
+ *
+ * @enum{number}
+ */
+export enum Gender {
+    Male,
+    Female,
+}
