37d197e9239bf06b0b3c5db43cf0cc4996ad8f00
Purpose <111461393+Purpose-Dev@users.noreply.github.com>
2024-12-18 13:11:45 +0100

    feat(events): add EventNames enum and export in events module
    ---
    Introduce `EventNames` enum for standardized event name management and export it through the events module for easier access.

---
+++ packages/client/src/events/EventNames.ts
@@ -0,0 +1,13 @@
+export enum EventNames {
+    CEventName = 'CEventName',
+    EntityDamaged = 'entityDamaged',
+    GameEventTriggered = 'gameEventTriggered',
+    MumbleConnected = 'mumbleConnected',
+    MumbleDisconnected = 'mumbleDisconnected',
+    OnClientResourceStart = 'onClientResourceStart',
+    OnClientResourceStop = 'onClientResourceStop',
+    OnResourceStart = 'onResourceStart',
+    OnResourceStarting = 'onResourceStarting',
+    OnResourceStop = 'onResourceStop',
+    PopulationPedCreating = 'populationPedCreating',
+}
