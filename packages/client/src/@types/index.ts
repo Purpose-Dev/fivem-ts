5ecf54003ea069ecf94d3b7f8f82ae704bcc8f07
Purpose <111461393+Purpose-Dev@users.noreply.github.com>
2024-12-19 20:55:41 +0100

    feat(types): add IDrawable interface
    ---
    Introduce the `IDrawable` interface with properties for position, size, color, and a `draw` method. Updated `index.ts` to export the new type.

--- packages/client/src/@types/index.ts
+++ packages/client/src/@types/index.ts
@@ -1,6 +1,7 @@
 export type { BlipData } from './BlipData';
 export type { EulerAngles } from './EulerAngles';
 export type { HandlingData } from './HandlingData';
+export type { IDrawable } from './IDrawable';
 export type { InvertAxis } from './InvertAxis';
 export type { Matrix } from './Matrix';
 export type { Vector } from './Vector';
