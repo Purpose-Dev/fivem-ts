9fa9665d4c6245434ac821f25e9d2f34bf33cffa
Purpose <111461393+Purpose-Dev@users.noreply.github.com>
2024-12-20 04:21:36 +0100

    feat(client): expand exports with new UI and game-related modules
    ---
    Added new UI components and game-related modules to the exports in `index.ts` files, including `LoadingPrompt`, `Screen`, `Camera`, `Game`, `ParticleEffect`, and more for enhanced functionality and modularity.

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -2,7 +2,18 @@ export * from './@types';
 export * from './enums';
 export * from './events';
 export * from './hashes';
+export * from './models';
+export * from './ui';
 export * from './utils';
 
 export { Audio } from './Audio';
 export { Blip } from './Blip';
+export { Camera } from './Camera';
+export { Checkpoint } from './Checkpoint';
+export { Game } from './Game';
+export { GameplayCamera } from './GameplayCamera';
+export { Model } from './Model';
+export { ParticleEffect } from './ParticleEffect';
+export { ParticleEffectAsset } from './ParticleEffectAsset';
+export { RelationshipGroup } from './RelationshipGroup';
+export { Rope } from './Rope';
