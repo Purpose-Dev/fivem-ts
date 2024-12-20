0a9da08b8c06547d2bdb163191542cdbe65fd398
Purpose <111461393+Purpose-Dev@users.noreply.github.com>
2024-12-20 03:31:42 +0100

    feat(particles): add ParticleEffect and ParticleEffectAsset classes
    Introduce reusable ParticleEffectAsset and ParticleEffect abstractions for managing and triggering particle effects, including support for looped and non-looped effects at specified coordinates or entities.

---
+++ packages/client/src/ParticleEffect.ts
@@ -0,0 +1,153 @@
+import { Vector3 } from '@fivem-ts/shared';
+import { Color, InvertAxis, InvertAxisFlags, ParticleEffectAsset } from '.';
+
+/**
+ *
+ * An abstract class representing a particle effect.
+ *
+ * This class provides methods and properties to manage and control
+ * various aspects of a particle effect such as its asset, effect name,
+ * offset, rotation, color, scale, range, and axis inversion.
+ */
+export abstract class ParticleEffect {
+    private handle: number;
+    protected readonly asset: ParticleEffectAsset;
+    protected readonly effectName: string;
+    protected offset: Vector3 = new Vector3(0, 0, 0);
+    protected rotation: Vector3 = new Vector3(0, 0, 0);
+    protected color: Color = Color.empty;
+    protected scale: number = 1.0;
+    protected range: number = 0.0;
+    protected invertAxis: InvertAxis = {
+        flags: InvertAxisFlags.None,
+    };
+
+    protected constructor(asset: ParticleEffectAsset, effectName: string) {
+        this.handle = -1;
+        this.asset = asset;
+        this.effectName = effectName;
+    }
+
+    public get Handle(): number {
+        return this.handle;
+    }
+
+    public set Handle(value: number) {
+        this.handle = value;
+    }
+
+    public get AssetName(): string {
+        return this.asset.AssetName;
+    }
+
+    public get EffectName(): string {
+        return this.effectName;
+    }
+
+    public get Offset(): Vector3 {
+        return this.offset;
+    }
+
+    public set Offset(offset: Vector3) {
+        this.offset = offset;
+    }
+
+    public get Rotation(): Vector3 {
+        return this.rotation;
+    }
+
+    public set Rotation(rotation: Vector3) {
+        this.rotation = rotation;
+        if (this.IsActive) {
+            const offset: Vector3 = this.Offset;
+            SetParticleFxLoopedOffsets(
+                this.Handle,
+                offset.x,
+                offset.y,
+                offset.z,
+                rotation.x,
+                rotation.y,
+                rotation.z,
+            );
+        }
+    }
+
+    public get Color(): Color {
+        return this.color;
+    }
+
+    public set Color(color: Color) {
+        this.color = color;
+    }
+
+    public get Scale(): number {
+        return this.scale;
+    }
+
+    public set Scale(scale: number) {
+        this.scale = scale;
+    }
+
+    public get Range(): number {
+        return this.range;
+    }
+
+    public set Range(range: number) {
+        this.range = range;
+        SetParticleFxLoopedRange(this.Handle, range);
+    }
+
+    public get InvertAxis(): InvertAxis {
+        return this.invertAxis;
+    }
+
+    public set InvertAxis(invertAxis: InvertAxis) {
+        this.invertAxis = invertAxis;
+        if (this.IsActive) {
+            this.stop();
+            this.start();
+        }
+    }
+
+    public get IsActive(): boolean {
+        return this.Handle !== -1 && DoesParticleFxLoopedExist(this.Handle);
+    }
+
+    public abstract start(): boolean;
+
+    /**
+     * Stops the particle effect if it is currently active.
+     * This method will remove the particle effect associated with the current handle.
+     *
+     * @returns {void}
+     */
+    public stop(): void {
+        if (this.IsActive) {
+            RemoveParticleFx(this.Handle, false);
+        }
+        this.Handle = -1;
+    }
+
+    /**
+     * Sets the value of a specific parameter for the particle effect.
+     *
+     * @param {string} parameterName - The name of the parameter to set.
+     * @param {number} value - The value to set for the specified parameter.
+     *
+     * @returns {void}
+     */
+    public setParameter(parameterName: string, value: number): void {
+        if (this.IsActive) {
+            SetParticleFxLoopedEvolution(this.Handle, parameterName, value, false);
+        }
+    }
+
+    /**
+     * Returns a string representation of the asset and effect names.
+     *
+     * @returns {string} A string combining the asset name and effect name separated by a backslash.
+     */
+    public toString(): string {
+        return `${this.AssetName}\\${this.EffectName}`;
+    }
+}
---
+++ packages/client/src/ParticleEffectAsset.ts
@@ -0,0 +1,270 @@
+import { Vector3 } from '@fivem-ts/shared';
+import { Entity, InvertAxisFlags } from '.';
+
+/**
+ * Represents a particle effect asset that can be used to manage and trigger particle
+ */
+export class ParticleEffectAsset {
+    constructor(private readonly assetName: string) {}
+
+    public get AssetName(): string {
+        return this.assetName;
+    }
+
+    public get IsLoaded(): boolean {
+        return HasNamedPtfxAssetLoaded(this.assetName);
+    }
+
+    public markAsNoLongerNeeded(): void {
+        RemoveNamedPtfxAsset(this.assetName);
+    }
+
+    public request(timeout: number): Promise<boolean> {
+        return new Promise(resolve => {
+            if (this.IsLoaded) {
+                resolve(this.IsLoaded);
+            } else {
+                RequestNamedPtfxAsset(this.assetName);
+                const start: number = GetGameTimer();
+                const interval: CitizenTimer = setInterval(() => {
+                    if (this.IsLoaded || GetGameTimer() - start >= timeout) {
+                        clearInterval(interval);
+                        resolve(this.IsLoaded);
+                    }
+                }, 0);
+            }
+        });
+    }
+
+    private setNextCall(): boolean {
+        if (!this.IsLoaded) {
+            RequestNamedPtfxAsset(this.assetName);
+        } else {
+            UseParticleFxAsset(this.assetName);
+            return true;
+        }
+
+        return false;
+    }
+
+    public startLoopedAtCoords(
+        effectName: string,
+        position: Vector3,
+        rotation = new Vector3(0, 0, 0),
+        scale = 1.0,
+        invertAxis = { flags: InvertAxisFlags.None },
+    ): boolean {
+        if (!this.setNextCall()) {
+            return false;
+        }
+
+        const invertAxisFlags: InvertAxisFlags = invertAxis.flags;
+        UseParticleFxAsset(this.assetName);
+        return Boolean(
+            StartParticleFxLoopedAtCoord(
+                effectName,
+                position.x,
+                position.y,
+                position.z,
+                rotation.x,
+                rotation.y,
+                rotation.z,
+                scale,
+                Boolean(invertAxisFlags & InvertAxisFlags.X),
+                Boolean(invertAxisFlags & InvertAxisFlags.Y),
+                Boolean(invertAxisFlags & InvertAxisFlags.Z),
+                false,
+            ),
+        );
+    }
+
+    public startNonLoopedAtCoords(
+        effectName: string,
+        position: Vector3,
+        rotation = new Vector3(0, 0, 0),
+        scale = 1.0,
+        invertAxis = { flags: InvertAxisFlags.None },
+    ): boolean {
+        if (!this.setNextCall()) {
+            return false;
+        }
+
+        const invertAxisFlags: InvertAxisFlags = invertAxis.flags;
+        UseParticleFxAsset(this.assetName);
+        return Boolean(
+            StartParticleFxNonLoopedAtCoord(
+                effectName,
+                position.x,
+                position.y,
+                position.z,
+                rotation.x,
+                rotation.y,
+                rotation.z,
+                scale,
+                Boolean(invertAxisFlags & InvertAxisFlags.X),
+                Boolean(invertAxisFlags & InvertAxisFlags.Y),
+                Boolean(invertAxisFlags & InvertAxisFlags.Z),
+            ),
+        );
+    }
+
+    public startNetworkedNonLoopedAtCoords(
+        effectName: string,
+        position: Vector3,
+        rotation = new Vector3(0, 0, 0),
+        scale = 1.0,
+        invertAxis = { flags: InvertAxisFlags.None },
+    ): boolean {
+        if (!this.setNextCall()) {
+            return false;
+        }
+
+        const invertAxisFlags: InvertAxisFlags = invertAxis.flags;
+        UseParticleFxAsset(this.assetName);
+        return StartNetworkedParticleFxNonLoopedAtCoord(
+            effectName,
+            position.x,
+            position.y,
+            position.z,
+            rotation.x,
+            rotation.y,
+            rotation.z,
+            scale,
+            Boolean(invertAxisFlags & InvertAxisFlags.X),
+            Boolean(invertAxisFlags & InvertAxisFlags.Y),
+            Boolean(invertAxisFlags & InvertAxisFlags.Z),
+        );
+    }
+
+    public startLoopedOnEntity(
+        effectName: string,
+        entity: Entity,
+        offset = new Vector3(0, 0, 0),
+        rotation = new Vector3(0, 0, 0),
+        scale = 1.0,
+        invertAxis = { flags: InvertAxisFlags.None },
+    ): boolean {
+        if (!this.setNextCall()) {
+            return false;
+        }
+
+        const invertAxisFlags: InvertAxisFlags = invertAxis.flags;
+        UseParticleFxAsset(this.assetName);
+        return Boolean(
+            StartParticleFxLoopedOnEntity(
+                effectName,
+                entity.Handle,
+                offset.x,
+                offset.y,
+                offset.z,
+                rotation.x,
+                rotation.y,
+                rotation.z,
+                scale,
+                Boolean(invertAxisFlags & InvertAxisFlags.X),
+                Boolean(invertAxisFlags & InvertAxisFlags.Y),
+                Boolean(invertAxisFlags & InvertAxisFlags.Z),
+            ),
+        );
+    }
+
+    public startNonLoopedOnEntity(
+        effectName: string,
+        entity: Entity,
+        offset = new Vector3(0, 0, 0),
+        rotation = new Vector3(0, 0, 0),
+        scale = 1.0,
+        invertAxis = { flags: InvertAxisFlags.None },
+    ): boolean {
+        if (!this.setNextCall()) {
+            return false;
+        }
+
+        const invertAxisFlags: InvertAxisFlags = invertAxis.flags;
+        UseParticleFxAsset(this.assetName);
+        return StartParticleFxNonLoopedOnEntity(
+            effectName,
+            entity.Handle,
+            offset.x,
+            offset.y,
+            offset.z,
+            rotation.x,
+            rotation.y,
+            rotation.z,
+            scale,
+            Boolean(invertAxisFlags & InvertAxisFlags.X),
+            Boolean(invertAxisFlags & InvertAxisFlags.Y),
+            Boolean(invertAxisFlags & InvertAxisFlags.Z),
+        );
+    }
+
+    public startNetworkedLoopedOnEntity(
+        effectName: string,
+        entity: Entity,
+        offset = new Vector3(0, 0, 0),
+        rotation = new Vector3(0, 0, 0),
+        scale = 1.0,
+        invertAxis = { flags: InvertAxisFlags.None },
+    ): boolean {
+        if (!this.setNextCall()) {
+            return false;
+        }
+
+        const invertAxisFlags: InvertAxisFlags = invertAxis.flags;
+        UseParticleFxAsset(this.assetName);
+        return Boolean(
+            StartNetworkedParticleFxLoopedOnEntity(
+                effectName,
+                entity.Handle,
+                offset.x,
+                offset.y,
+                offset.z,
+                rotation.x,
+                rotation.y,
+                rotation.z,
+                scale,
+                Boolean(invertAxisFlags & InvertAxisFlags.X),
+                Boolean(invertAxisFlags & InvertAxisFlags.Y),
+                Boolean(invertAxisFlags & InvertAxisFlags.Z),
+            ),
+        );
+    }
+
+    public startNetworkedNonLoopedOnEntity(
+        effectName: string,
+        entity: Entity,
+        offset = new Vector3(0, 0, 0),
+        rotation = new Vector3(0, 0, 0),
+        scale = 1.0,
+        invertAxis = { flags: InvertAxisFlags.None },
+    ): boolean {
+        if (!this.setNextCall()) {
+            return false;
+        }
+
+        const invertAxisFlags: InvertAxisFlags = invertAxis.flags;
+        UseParticleFxAsset(this.assetName);
+        return StartNetworkedParticleFxNonLoopedOnEntity(
+            effectName,
+            entity.Handle,
+            offset.x,
+            offset.y,
+            offset.z,
+            rotation.x,
+            rotation.y,
+            rotation.z,
+            scale,
+            Boolean(invertAxisFlags & InvertAxisFlags.X),
+            Boolean(invertAxisFlags & InvertAxisFlags.Y),
+            Boolean(invertAxisFlags & InvertAxisFlags.Z),
+        );
+    }
+
+    public toString(): string {
+        return this.assetName;
+    }
+
+    public toJson(): string {
+        return JSON.stringify({ assetName: this.assetName });
+    }
+}
